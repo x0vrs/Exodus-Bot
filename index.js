e03344d6c72d9a07f8c98f19f91be0f759c0e89d
x0vrs <85258678+x0vrs@users.noreply.github.com>
2021-06-04 02:58:32 +0100

    chaser

--- index.js
+++ index.js
@@ -36,7 +36,22 @@ const prefix = '£'
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}.`));
 
+function is_url(str) {
+  let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if(regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
 client.on('message', message => {
+
+	if(is_url(message.content) === true) {
+		message.delete()
+		return message.channel.send("Links are prohibited in this channel.")
+    }
 
 	const Embed = new Discord.MessageEmbed()
 		.setColor('#000000')
